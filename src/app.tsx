8a57a45b2986a6a673d7ed5d6fc10f96635b5357
RickyOwings <rickyowings@gmail.com>
2023-07-25 17:36:46 -0400

    3d model section

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,9 @@
 import React, {Component, useState} from "react";
 import { ReactDOM } from "react";
 import Homepage from './homepage';
-import Music, {VolumeSlider} from './music';
-import Navbar, {NavbarHrefLocal, SiteChange} from './navbar';
+import Music, { VolumeSlider } from './music';
+import Modeling from './modeling'
+import Navbar, { NavbarHrefLocal, SiteChange } from './navbar';
 
 let page = "homepage";
 interface ContentProps {
@@ -15,12 +16,17 @@ export default function () {
     const links = {
         homepage: [
             <NavbarHrefLocal key="aboutme" href="#aboutme">About Me</NavbarHrefLocal>,
-            <SiteChange callback={toMusic} key="tomusic">To My Music</SiteChange>
+            <SiteChange callback={toMusic} key="tomusic">To My Music</SiteChange>,
+            <SiteChange callback={toModeling} key="tomodeling">To 3d Models</SiteChange>
         ],
         music: [
             <NavbarHrefLocal key="about" href="#about">About</NavbarHrefLocal>,
             <SiteChange callback={toHomepage} key="tohomepage">To Homepage</SiteChange>,
             <VolumeSlider></VolumeSlider>
+        ],
+        modeling: [
+            <SiteChange callback={toHomepage} key="tohomepage">To Homepage</SiteChange>,
+
         ],
     }
     var [currentLinks, setLinks] = useState(links.homepage)
@@ -29,9 +35,15 @@ export default function () {
         setContent(<Music></Music>);
         setLinks(links.music);
     }
+
     function toHomepage(){
         setContent(<Homepage></Homepage>);
         setLinks(links.homepage);
+    }
+
+    function toModeling(){
+        setContent(<Modeling></Modeling>);
+        setLinks(links.modeling);
     }
 
 
