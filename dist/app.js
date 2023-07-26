de0a9e00050a98aabee685b619a3a71efd86eb5b
RickyOwings <110073156+RickyOwings@users.noreply.github.com>
2023-07-26 15:15:43 +0000

    Deploying to gh-pages from @ RickyOwings/portfolio-page@4f82b1ae1c6cf03b6e5c5a28406aad0b2a8f88d2 🚀

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,7 @@
 import React, {useState} from "../snowpack/pkg/react.js";
 import Homepage from "./homepage.js";
 import Music, {VolumeSlider} from "./music.js";
+import Modeling from "./modeling.js";
 import Navbar, {NavbarHrefLocal, SiteChange} from "./navbar.js";
 let page = "homepage";
 export default function() {
@@ -13,7 +14,11 @@ export default function() {
       /* @__PURE__ */ React.createElement(SiteChange, {
         callback: toMusic,
         key: "tomusic"
-      }, "To My Music")
+      }, "To My Music"),
+      /* @__PURE__ */ React.createElement(SiteChange, {
+        callback: toModeling,
+        key: "tomodeling"
+      }, "To 3d Models")
     ],
     music: [
       /* @__PURE__ */ React.createElement(NavbarHrefLocal, {
@@ -25,6 +30,12 @@ export default function() {
         key: "tohomepage"
       }, "To Homepage"),
       /* @__PURE__ */ React.createElement(VolumeSlider, null)
+    ],
+    modeling: [
+      /* @__PURE__ */ React.createElement(SiteChange, {
+        callback: toHomepage,
+        key: "tohomepage"
+      }, "To Homepage")
     ]
   };
   var [currentLinks, setLinks] = useState(links.homepage);
@@ -36,6 +47,10 @@ export default function() {
   function toHomepage() {
     setContent(/* @__PURE__ */ React.createElement(Homepage, null));
     setLinks(links.homepage);
+  }
+  function toModeling() {
+    setContent(/* @__PURE__ */ React.createElement(Modeling, null));
+    setLinks(links.modeling);
   }
   return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement(Navbar, {
     links: currentLinks
---
+++ dist/modeling.js
@@ -0,0 +1,53 @@
+import React from "../snowpack/pkg/react.js";
+const targetWidth = 800;
+const targetHeight = 9 * targetWidth / 16;
+const HBotID = `hoverbot`;
+const HoverBot = /* @__PURE__ */ React.createElement("div", {
+  className: "center-elements sec",
+  dangerouslySetInnerHTML: {__html: `<div class="sketchfab-embed-wrapper"> <iframe title="Alarm Clock" id="${HBotID}" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/39b75fde8f2641c8bef97b7b50d699ff/embed"> </iframe> <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> <a href="https://sketchfab.com/3d-models/alarm-clock-8bf6eaa83c6c40b8a933bb94b731a1a2?utm_medium=embed&utm_campaign=share-popup&utm_content=8bf6eaa83c6c40b8a933bb94b731a1a2" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> Hover Bot </a> by <a href="https://sketchfab.com/rickyowings?utm_medium=embed&utm_campaign=share-popup&utm_content=8bf6eaa83c6c40b8a933bb94b731a1a2" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> Ricky Owings </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=8bf6eaa83c6c40b8a933bb94b731a1a2" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a></p></div>`}
+});
+const AClockID = `alarmclock`;
+const AlarmClock = /* @__PURE__ */ React.createElement("div", {
+  className: "center-elements sec",
+  dangerouslySetInnerHTML: {__html: `<div class="sketchfab-embed-wrapper"> <iframe title="Alarm Clock" id="${AClockID}" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/8bf6eaa83c6c40b8a933bb94b731a1a2/embed"> </iframe> <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> <a href="https://sketchfab.com/3d-models/alarm-clock-8bf6eaa83c6c40b8a933bb94b731a1a2?utm_medium=embed&utm_campaign=share-popup&utm_content=8bf6eaa83c6c40b8a933bb94b731a1a2" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> Alarm Clock </a> by <a href="https://sketchfab.com/rickyowings?utm_medium=embed&utm_campaign=share-popup&utm_content=8bf6eaa83c6c40b8a933bb94b731a1a2" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> Ricky Owings </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=8bf6eaa83c6c40b8a933bb94b731a1a2" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a></p></div>`}
+});
+const AChairID = `armchair`;
+const ArmChair = /* @__PURE__ */ React.createElement("div", {
+  className: "center-elements sec",
+  dangerouslySetInnerHTML: {__html: `<div class="sketchfab-embed-wrapper"> <iframe title="Armchair" id="${AChairID}" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/ec6834dc6fcf471988769abe0fbd735a/embed"> </iframe> <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> <a href="https://sketchfab.com/3d-models/armchair-ec6834dc6fcf471988769abe0fbd735a?utm_medium=embed&utm_campaign=share-popup&utm_content=ec6834dc6fcf471988769abe0fbd735a" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> Armchair </a> by <a href="https://sketchfab.com/rickyowings?utm_medium=embed&utm_campaign=share-popup&utm_content=ec6834dc6fcf471988769abe0fbd735a" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> Ricky Owings </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=ec6834dc6fcf471988769abe0fbd735a" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a></p></div>`}
+});
+const TankID = `tank`;
+const TigerTank = /* @__PURE__ */ React.createElement("div", {
+  className: "center-elements sec",
+  dangerouslySetInnerHTML: {__html: `<div class="sketchfab-embed-wrapper"> <iframe title="Low Poly Tiger 1" id="${TankID}" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/3abb79fa3a2d4c098e42d2b2c940ec34/embed"> </iframe> <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> <a href="https://sketchfab.com/3d-models/low-poly-tiger-1-3abb79fa3a2d4c098e42d2b2c940ec34?utm_medium=embed&utm_campaign=share-popup&utm_content=3abb79fa3a2d4c098e42d2b2c940ec34" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> Low Poly Tiger 1 </a> by <a href="https://sketchfab.com/rickyowings?utm_medium=embed&utm_campaign=share-popup&utm_content=3abb79fa3a2d4c098e42d2b2c940ec34" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> Ricky Owings </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=3abb79fa3a2d4c098e42d2b2c940ec34" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a></p></div>`}
+});
+const IDArr = [HBotID, AClockID, AChairID, TankID];
+function resizeElements() {
+  window.addEventListener("resize", () => {
+    resizeFunction();
+  });
+  window.addEventListener("deviceorientation", () => {
+    resizeFunction();
+  });
+}
+function resizeFunction() {
+  const maxWidth = window.innerWidth;
+  IDArr.forEach((id) => {
+    const element = document.getElementById(id);
+    if (!element)
+      return;
+    const width = targetWidth < maxWidth ? targetWidth : maxWidth;
+    const height = targetHeight * width / targetWidth;
+    element.style.width = `${width}px`;
+    element.style.height = `${height}px`;
+  });
+}
+export default function() {
+  resizeElements();
+  resizeFunction();
+  return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("div", {
+    className: "navbar-spacer"
+  }), /* @__PURE__ */ React.createElement("div", {
+    className: "center-elements"
+  }, /* @__PURE__ */ React.createElement("h5", null, "My 3D Models"), HoverBot, AlarmClock, ArmChair, TigerTank));
+}
